590e36fa47941fc327b85bd35ee2b0be884f5444
Prasad Patra <prasadpatra234@gmail.com>
2026-05-06 21:52:38 +0530

    use of modules

--- Scripts/amazon.js
+++ Scripts/amazon.js
@@ -1,3 +1,5 @@
+import {cart} from '../data/cart.js';
+
 let productsHTML = '';
 products.forEach((product) => {
     productsHTML +=`
@@ -24,7 +26,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -60,6 +62,9 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
     button.addEventListener('click',()=>{
        const productId = button.dataset.productId ;
 
+       const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+       const quantity = Number(quantitySelector.value);
+
        let matchingItem;
        cart.forEach((item)=>{
         if(productId === item.productId){
@@ -68,12 +73,12 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
        });
 
        if (matchingItem){
-        matchingItem.quantity +=1;
+        matchingItem.quantity += quantity;
        }
        else{
         cart.push({
         productId : productId,
-        quantity :1
+        quantity : quantity
        });
        }
 
